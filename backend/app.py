0fa15ec593cbf5767d138825da2bd43961644f77
lasya-02 <vellampallilasya@gmail.com>
2025-12-03 19:26:36 -0500

    testing

--- backend/app.py
+++ backend/app.py
@@ -27,7 +27,8 @@ Instrumentator().instrument(app).expose(app, endpoint="/metrics")
 # CORS for React
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","http://127.0.0.1:8000", "*"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","http://127.0.0.1:8000","http://54.197.27.81",  # optional if accessing by IP
+        "http://ec2-54-197-27-81.compute-1.amazonaws.com","*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
