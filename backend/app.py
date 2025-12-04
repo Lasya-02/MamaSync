15f3286028f2d49b58a6ab0f6d8d4ba104631b59
lasya-02 <vellampallilasya@gmail.com>
2025-12-03 20:44:14 -0500

    testing

--- backend/app.py
+++ backend/app.py
@@ -28,7 +28,7 @@ Instrumentator().instrument(app).expose(app, endpoint="/metrics")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","http://127.0.0.1:8000","http://54.197.27.81:8000",  # optional if accessing by IP
-        "http://ec2-54-197-27-81.compute-1.amazonaws.com:8000","http://ec2-54-197-27-81.compute-1.amazonaws.com","http://54.197.27.81","*"],
+        "http://ec2-54-197-27-81.compute-1.amazonaws.com:8000","http://ec2-54-197-27-81.compute-1.amazonaws.com","http://54.197.27.81"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
